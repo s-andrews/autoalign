877106dd2ca81e96fda8e105dbe1d35429f6009b
Simon Andrews <simon.andrews@babraham.ac.uk>
2024-04-05 12:24:01 +0100

    Added file upload processing

--- www/autoalign.py
+++ www/autoalign.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, render_template, make_response
+from flask import Flask, request, render_template, make_response, redirect, url_for
+from werkzeug.utils import secure_filename
 import random
 from urllib.parse import quote_plus
 from pathlib import Path
 import json
-import time
+import subprocess
 
 app = Flask(__name__)
 
@@ -15,6 +16,45 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/start_alignment", methods=["GET","POST"])
+def start_alignment():
+
+    # Let's make a folder to send the data to
+    run_id = generate_id(20)
+
+    output_path = Path(server_conf["data_folder"])/run_id
+
+    if output_path.exists():
+        raise Exception("Run ID already exists, what are the chances of that.")
+    
+    output_path.mkdir()
+    
+
+    files = request.files
+    reference = files["reference"]
+    fastq = files["fastq"]
+
+    reference_filename = secure_filename(reference.filename)
+    reference.save(output_path / reference_filename)
+
+    fastq_filename = secure_filename(fastq.filename)
+    fastq.save(output_path / fastq_filename)
+
+    subprocess.run(["../process_run.py",run_id], check=True)
+
+    return redirect(url_for("view_results/"+run_id))
+
+
+@app.route("/view_results/<job_id>")
+def view_results(job_id):
+
+    fileit = (Path(server_conf["output_folder"]) / job_id).iterdir()
+    files = []
+
+    for file in fileit:
+        files.append(file.name)
+
+    return render_template("results.html", job_id=job_id, files=files, baseurl=server_conf["output_url"])
 
 def get_form():
     if request.method == "GET":
